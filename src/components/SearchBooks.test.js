226b284c0392671ddc30f59243e9f4cf68e07131
Renato Medina <renato.medina@m4u.com.br>
2017-12-12 16:54:44 -0200

    Refactoring SearchBook tests

--- src/components/SearchBooks.test.js
+++ src/components/SearchBooks.test.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import SearchBooks from './SearchBooks';
-
+import { MemoryRouter } from 'react-router-dom'
+import { Debounce } from 'react-throttle'
+import book from '../__mocks__/BookItem'
 
 describe('<SearchBooks />', () => {
-    it('shallow renders correctly', () => {
-      expect(shallow(<SearchBooks />))
-    })
 
-    xit('mount renders correctly', () => {
-      expect(mount(<SearchBooks />))
+  let props = {
+    libaryBooks: [book],
+    addBookHandler: jest.fn()
+  }
+
+  const shallowSubject = () => {
+    return shallow(<SearchBooks {...props} />)
+  }
+
+  const mountSubject = () => {
+    return mount(<MemoryRouter><SearchBooks {...props} /></MemoryRouter>)
+  }
+
+  it('shallow renders correctly', () => {
+    expect(shallowSubject())
+  })
+
+  it('mount renders correctly', () => {
+    expect(mountSubject())
+  })
+
+  it('has a time interval of 400ms', () => {
+    expect(shallowSubject().find(Debounce).prop('time')).toBe('400')
+  })
+
+  it('is fired when the user types on the search input', () => {
+    expect(shallowSubject().find(Debounce).prop('handler')).toBe('onChange')
+  })
+
+  describe('Search input', () => {
+    const input = () => mountSubject().find('input')
+    
+    it('is rendered', () => {
+      expect(input().exists()).toBe(true)
     })
+  })
 })
